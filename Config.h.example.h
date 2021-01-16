383fbb4cb19bcb7747ebd54bcb8dc7d18337c91a
Sprakle <ben.cracknell.96@gmail.com>
2021-01-16 12:44:44 -0800

    Added example config file

---
+++ Config.h.example.h
@@ -0,0 +1,10 @@
+#define WLAN_SSID        "wifi"
+#define WLAN_PASS        "password"
+
+#define MQTT_SERVER      "192.168.1.xxx"
+#define MQTT_SERVERPORT  1883
+#define MQTT_USERNAME    "username"
+#define MQTT_KEY         "password"
+
+#define DEVICE_NAME      "office-shop-light"
+#define DEVICE_DISPLAY   "Office Shop Light"
